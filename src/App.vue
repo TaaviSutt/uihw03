d07f01c6b3d5d6ddab72b94faaa7fbc87a7ede68
Taavi.Sutt <taavi.sutt@ttu.ee>
2018-04-26 22:17:36 +0300

    Emptied app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <Stepper class="form-screen" v-on:complete="complete" v-if="!formComplete"/>
-    <SuccessScreen ref="success"/>
   </div>
 </template>
 <script>
@@ -10,8 +8,6 @@ import Vue from 'vue'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import VueMaterial from 'vue-material'
-import Stepper from './components/Stepper'
-import SuccessScreen from './components/SuccessScreen'
 
 import './scss/App.scss'
 
@@ -19,17 +15,13 @@ Vue.use(VueMaterial);
 export default {
   name: 'App',
   components: {
-    Stepper,
-    SuccessScreen
+
   },
   data: () => ({
-    formComplete: false
+
   }),
   methods: {
-    complete() {
-      this.formComplete = true;
-      this.$refs.success.initializeEffect();
-    }
+
   }
 }
 </script>
