b899bc10d2bc88f182b53e81f18a0ef678a97c29
Taavi.Sutt <taavi.sutt@ttu.ee>
2018-04-27 11:13:38 +0300

    Iframe component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="app">
+    <Iframe></Iframe>
   </div>
 </template>
 <script>
 
-import Vue from 'vue'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-import VueMaterial from 'vue-material'
+  import Vue from 'vue'
+  import 'vue-material/dist/vue-material.min.css'
+  import 'vue-material/dist/theme/default.css'
+  import VueMaterial from 'vue-material'
+  import Iframe from './components/Iframe'
 
-import './scss/App.scss'
+  import './scss/App.scss'
 
-Vue.use(VueMaterial);
-export default {
-  name: 'App',
-  components: {
+  Vue.use(VueMaterial);
+  export default {
+    name: 'App',
+    components: {
+      Iframe
 
-  },
-  data: () => ({
-
-  }),
-  methods: {
-
+    },
+    data: () => ({}),
+    methods: {}
   }
-}
 </script>
---
+++ src/components/Iframe.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>
+      <md-field>
+        <label>Url aadress</label>
+        <md-input name="url-address" id="url-address" v-model.trim="urlAddress"/>
+      </md-field>
+    </div>
+    <div class="scalable scalable-16-9">
+      <div class="scalable-content">
+        <iframe :src="urlAddress" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Iframe',
+    data: () => ({
+      urlAddress: '',
+    }),
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .scalable {
+    overflow: hidden;
+  }
+
+  .scalable iframe {
+    height: 75%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 50%;
+  }
+
+  .scalable .scalable-content {
+    height: 0;
+    position: relative;
+  }
+
+  .scalable-16-9 .scalable-content {
+    /* Percentage from 9 divided by 16 for ratio of 16:9. */
+    padding-bottom: 56.25%;
+  }
+
+</style>
